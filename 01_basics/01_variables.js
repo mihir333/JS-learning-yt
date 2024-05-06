517f81d81c482ab40d438957e8761e12779cfb95
Mihir Malviya <68920654+mihir333@users.noreply.github.com>
2024-05-06 12:04:23 +0000

    dataTypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12455
+let accountEmail = "malviya333.mihir@gmail.com"
+var accountPassword = "kohli"
+accountCity = "Pune"
+
+accountEmail = "mihir@gmail.com"
+accountPassword = "gambhir"
+accountCity = "Kota"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
